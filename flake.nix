b12eb75c3db008bead40cdb9bcf7160fc7a41ede
Fredrik Gjellestad <fredrik@nerdiverset.no>
2026-03-17 22:12:10 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            (python3.withPackages (ps:
+              with ps; [
+                cryptography
+                debugpy
+                imagehash
+                pillow
+                pip
+                requests
+              ]))
+            git
+            nmap
+          ];
+
+          shellHook = ''
+            exam shell with packages that might be needed
+          '';
+        };
+      }
+    );
+}
